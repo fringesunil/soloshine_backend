ceca2c50c2467524180cce427af8811d8bbc37de
Sreeshks <sreeshksureshh@gmail.com>
2025-09-11 10:03:19 +0530

    update jwt expired

--- controller/authController.js
+++ controller/authController.js
@@ -15,7 +15,7 @@ const login = async (req, res) => {
    }
    const passwordMatch =  bcrypt.compareSync(password,user.password)
    if(passwordMatch){
-        const token = jwt.sign({ _id: user._id,email: user.email,role:user.role }, process.env.JWT_SECRET_KEY,{expiresIn: '6h'});
+        const token = jwt.sign({ _id: user._id,email: user.email,role:user.role }, process.env.JWT_SECRET_KEY,{expiresIn: '30d'});
         res.status(200).json(
            {
              success: true,
