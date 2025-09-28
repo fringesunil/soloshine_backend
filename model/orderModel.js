893272cb30e93529b0f270d5f938da756d94fb03
Fringe sunil <fringeks011@gmail.com>
2025-09-28 20:34:18 +0530

    Updated the status Enum

--- model/orderModel.js
+++ model/orderModel.js
@@ -18,7 +18,7 @@ const orderSchema = new mongoose.Schema({
     },
     status: {
         type: String,
-        enum: ['Pending', 'In Progress', 'Completed', 'Cancelled', 'Due', 'Delay', 'Partial Delivery'],
+        enum: ['Pending', 'In Progress', 'Completed', 'Cancelled', 'Due', 'Delay', 'Partial Delivery','Wax','Casted','D-C'],
         default: 'Pending'
     },
 
