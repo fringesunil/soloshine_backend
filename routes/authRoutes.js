ad27ff74b71ffb3c5d7bd3ac773c51af143c8797
Fringe sunil <fringeks011@gmail.com>
2025-11-27 22:37:18 +0530

    Add ping

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,7 +8,13 @@ const router = express.Router();
 
 router.post('/login', login);
 
-
+router.get('/ping',(req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Server is active",
+    timestamp: new Date()
+  });
+})
 
 
 module.exports = router; 
