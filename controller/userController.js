500592ecd5ec1678c19d253110ca4f69ae19cd1c
EqsoftSreesh <eqsoftsreesh@gmail.com>
2026-01-29 15:45:01 +0530

    forget pass api added

--- controller/userController.js
+++ controller/userController.js
@@ -1,162 +1,196 @@
-const User= require('../model/userModel')
+const User = require('../model/userModel')
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 const getAlluser = async (req, res) => {
-    try {
-      const user = await User.find().exec();
-      const response = user.map(user => ({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        phone: user.phone,
-        role: user.role,
-        address: user.address,
-        fcmtoken: user.fcmtoken
-      }));
-      res.status(200).json({
-        success: true,
-        data: response,
-        message: "User retrieved successfully"
-      });
-    } catch (error) {
-      res.status(500).json({
-        success: false,
-        data: null,
-        message: "Server error",
-        error: error.message
-      });
-    }
+  try {
+    const user = await User.find().exec();
+    const response = user.map(user => ({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      phone: user.phone,
+      role: user.role,
+      address: user.address,
+      fcmtoken: user.fcmtoken
+    }));
+    res.status(200).json({
+      success: true,
+      data: response,
+      message: "User retrieved successfully"
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      data: null,
+      message: "Server error",
+      error: error.message
+    });
   }
+}
 
 const getUserbyid = async (req, res) => {
-    try {
-      const user = await User.findById(req.params.userid).exec();
-      if (!user) {
-        return res.status(404).json({
-          success: false,
-          data: null,
-          message: "User not found"
-        });
-      }
-      const response = {
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        phone: user.phone,
-        role: user.role,
-        address: user.address,
-         fcmtoken: user.fcmtoken
-      };
-      res.status(200).json({
-        success: true,
-        data: response,
-        message: "User retrieved successfully"
-      });
-    } catch (error) {
-      res.status(500).json({
+  try {
+    const user = await User.findById(req.params.userid).exec();
+    if (!user) {
+      return res.status(404).json({
         success: false,
         data: null,
-        message: "Server error",
-        error: error.message
+        message: "User not found"
       });
     }
+    const response = {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      phone: user.phone,
+      role: user.role,
+      address: user.address,
+      fcmtoken: user.fcmtoken
+    };
+    res.status(200).json({
+      success: true,
+      data: response,
+      message: "User retrieved successfully"
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      data: null,
+      message: "Server error",
+      error: error.message
+    });
   }
-  
-  const addUser = async (req, res) => {
-    try {
-      const { email, password, ...data } = req.body;
-      const existingUser = await User.findOne({ email });
-      if (existingUser) {
-        return res.status(400).json({ message: 'Email already exists' });
-      }
-     
-      const hash = bcrypt.hashSync(password, saltRounds);
-      
-      const user = new User({
-        ...data,
-        email, 
-        password: hash,
-      });
-      await user.save();
-      const response = {
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        phone: user.phone,
-        role: user.role,
-        address: user.address,
-        fcmtoken: user.fcmtoken
-      };
-    
-     res.status(200).json({
+}
+
+const addUser = async (req, res) => {
+  try {
+    const { email, password, ...data } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'Email already exists' });
+    }
+
+    const hash = bcrypt.hashSync(password, saltRounds);
+
+    const user = new User({
+      ...data,
+      email,
+      password: hash,
+    });
+    await user.save();
+    const response = {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      phone: user.phone,
+      role: user.role,
+      address: user.address,
+      fcmtoken: user.fcmtoken
+    };
+
+    res.status(200).json({
       success: true,
       data: response,
       message: "User added successfully"
     });
-    } catch (error) {
-      res.status(500).json({ message: 'Server error', error });
-    }
-  };
-  
- 
- const updateUser = async (req, res) => {
-    try {
-      const updateuser = await User.findByIdAndUpdate(req.params.userid, req.body, {new:true})
-      if (!updateuser) {
-        return res.status(404).json({
-          success: false,
-          data: null,
-          message: "User not found"
-        });
-      }
-      const response = {
-        _id: updateuser._id,
-        name: updateuser.name,
-        email: updateuser.email,
-        phone: updateuser.phone,
-        role: updateuser.role,
-        address: updateuser.address,
-        fcmtoken: updateuser.fcmtoken
-      };
-      res.status(200).json({
-        success: true,
-        data: response,
-        message: "User updated successfully"
-      });
-    } catch (error) {
-      res.status(500).json({
-        success: false,
-        data: null,
-        message: "Server error",
-        error: error.message
-      });
-    }
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error });
   }
-  
- const deleteUser = async (req, res) => {
-    try {
-      await User.findByIdAndDelete(req.params.userid);
-      res.status(200).json({
-        success: true,
-        data: null,
-        message: "User deleted successfully"
-      });
-    } catch (error) {
-      res.status(500).json({
-        success: false,
-        data: null,
-        message: "Server error",
-        error: error.message
-      });
-    }
-  }
+};
 
 
-  module.exports={
-    getAlluser,
-    getUserbyid,
-    addUser,
-    updateUser,
-    deleteUser
+const updateUser = async (req, res) => {
+  try {
+    const updateuser = await User.findByIdAndUpdate(req.params.userid, req.body, { new: true })
+    if (!updateuser) {
+      return res.status(404).json({
+        success: false,
+        data: null,
+        message: "User not found"
+      });
+    }
+    const response = {
+      _id: updateuser._id,
+      name: updateuser.name,
+      email: updateuser.email,
+      phone: updateuser.phone,
+      role: updateuser.role,
+      address: updateuser.address,
+      fcmtoken: updateuser.fcmtoken
+    };
+    res.status(200).json({
+      success: true,
+      data: response,
+      message: "User updated successfully"
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      data: null,
+      message: "Server error",
+      error: error.message
+    });
   }
+}
+
+const deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.userid);
+    res.status(200).json({
+      success: true,
+      data: null,
+      message: "User deleted successfully"
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      data: null,
+      message: "Server error",
+      error: error.message
+    });
+  }
+}
+
+
+const forgetPassword = async (req, res) => {
+  try {
+    const { email, newPassword } = req.body;
+    if (!email || !newPassword) {
+      return res.status(400).json({ message: 'Email and new password are required' });
+    }
+    const user = await User.findOne({ email }).exec();
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        data: null,
+        message: "User not found"
+      });
+    }
+    const hash = bcrypt.hashSync(newPassword, saltRounds);
+    user.password = hash;
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      data: null,
+      message: "Password updated successfully"
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      data: null,
+      message: "Server error",
+      error: error.message
+    });
+  }
+}
+
+module.exports = {
+  getAlluser,
+  getUserbyid,
+  addUser,
+  updateUser,
+  deleteUser,
+  forgetPassword
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,19 +1,21 @@
 const express = require('express');
-const { getAlluser, getUserbyid, addUser, updateUser, deleteUser } = require('../controller/userController');
+const { getAlluser, getUserbyid, addUser, updateUser, deleteUser, forgetPassword } = require('../controller/userController');
 const { authenticateToken, checkAdmin } = require('../middleware/authToken');
 
 
 const router = express.Router();
 
-router.get('/', authenticateToken,getAlluser);
+router.get('/', authenticateToken, getAlluser);
 
-router.get('/:userid',authenticateToken, getUserbyid);
+router.get('/:userid', authenticateToken, getUserbyid);
 
 router.post('/', addUser);
 
-router.patch('/:userid',authenticateToken, updateUser);
+router.post('/forget-password', forgetPassword);
 
-router.delete('/:userid',authenticateToken,checkAdmin, deleteUser);
+router.patch('/:userid', authenticateToken, updateUser);
+
+router.delete('/:userid', authenticateToken, checkAdmin, deleteUser);
 
 
 module.exports = router; 
